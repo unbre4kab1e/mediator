2a56b0ae62b963d22f05847a04071aacc21c416d
unbre4kab1e <moujahiddine@gmail.com>
2024-07-28 16:54:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,20 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+authenticator.login()
 # Show app title and description.
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
 st.title("🎫 Support tickets")
